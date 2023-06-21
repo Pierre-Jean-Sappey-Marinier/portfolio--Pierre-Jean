70f45941f81e76afd661cd89b5cd32b390aebed8
Pierre-Jean-Sappey-Marinier <113429394+Pierre-Jean-Sappey-Marinier@users.noreply.github.com>
2023-06-21 18:00:58 +0200

    essai basePath next

---
+++ server/flight-server-css-manifest.js
@@ -0,0 +1,29 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "E:\\Projects\\Portfolio v1.1\\src\\app\\page.js": [
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\page.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Skills\\skills.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Projects\\Projects.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Footer\\Footer.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Biographie\\Biographie.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\BoardSkills\\BoardSkill.module.scss"
+    ],
+    "E:\\Projects\\Portfolio v1.1\\src\\app\\layout.js": [
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\globals.scss",
+      "E:\\Projects\\Portfolio v1.1\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "E:\\Projects\\Portfolio v1.1\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Montserrat\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"montserrat\"}"
+    ]
+  },
+  "cssModules": {
+    "E:\\Projects\\Portfolio v1.1\\src\\app\\page": [
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Biographie\\Biographie.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Projects\\Projects.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Skills\\skills.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\Footer\\Footer.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\page.module.scss",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\globals.scss",
+      "E:\\Projects\\Portfolio v1.1\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Montserrat\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"montserrat\"}",
+      "E:\\Projects\\Portfolio v1.1\\src\\app\\_component\\BoardSkills\\BoardSkill.module.scss"
+    ]
+  }
+}
